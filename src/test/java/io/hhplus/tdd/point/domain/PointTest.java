fc641f30733cc719783991575a76a76a3adbbbaf
hwangdaesun <hds650588@gmail.com>
2025-10-19 14:47:37 +0900

    test: 음수 금액 충전 시 예외 처리 테스트 수정

--- src/test/java/io/hhplus/tdd/point/domain/PointTest.java
+++ src/test/java/io/hhplus/tdd/point/domain/PointTest.java
@@ -2,6 +2,7 @@ package io.hhplus.tdd.point.domain;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,10 +11,10 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class PointTest {
 
-    @DisplayName("음수 금액 또는 0원으로 충전하면 false를 반환한다")
+    @DisplayName("음수 금액 또는 0원으로 충전하면 예외를 발생시킨다")
     @ParameterizedTest
     @ValueSource(longs = {-1000L, -500L, -100L, -1L, 0L})
-    void charge_WithNegativeAmount_ReturnsFalse(long invalidAmount) {
+    void charge_WithNegativeAmount_ThrowsException(long invalidAmount) {
         // given
         long currentAmount = 1000L;
         Member member = new Member(1);
