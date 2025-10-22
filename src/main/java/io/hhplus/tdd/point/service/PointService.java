f98980d89e4d147d8da2c8e6120d433b1032ea38
hwangdaesun <hds650588@gmail.com>
2025-10-22 23:05:40 +0900

    feat: ReentrantLock 기반 동시성 처리 로직 추가
    - 사용자 ID별로 Lock 할당

--- src/main/java/io/hhplus/tdd/point/service/PointService.java
+++ src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -7,6 +7,9 @@ import io.hhplus.tdd.point.domain.Member;
 import io.hhplus.tdd.point.domain.Point;
 import io.hhplus.tdd.point.dto.PointHistory;
 import io.hhplus.tdd.point.dto.UserPoint;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,14 +21,22 @@ public class PointService {
 
     private final PointHistoryTable pointHistoryTable;
     private final UserPointTable userPointTable;
+    private final Map<Long, ReentrantLock> userLocks = new ConcurrentHashMap<>();
 
-    public UserPoint charge(long id, long amount){
-        UserPoint userPoint = userPointTable.selectById(id);
-        Point point = new Point(new Member(userPoint.id()), userPoint.point());
-        long chargedPoint = point.charge(amount);
-        UserPoint updatedUserPoint = userPointTable.insertOrUpdate(id, chargedPoint);
-        pointHistoryTable.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
-        return updatedUserPoint;
+    public UserPoint charge(long id, long amount) {
+        ReentrantLock lock = userLocks.computeIfAbsent(id, key -> new ReentrantLock());
+
+        lock.lock();
+        try {
+            UserPoint userPoint = userPointTable.selectById(id);
+            Point point = new Point(new Member(userPoint.id()), userPoint.point());
+            long chargedPoint = point.charge(amount);
+            UserPoint updatedUserPoint = userPointTable.insertOrUpdate(id, chargedPoint);
+            pointHistoryTable.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
+            return updatedUserPoint;
+        } finally {
+            lock.unlock();
+        }
     }
 
     public UserPoint use(long id, long amount){
