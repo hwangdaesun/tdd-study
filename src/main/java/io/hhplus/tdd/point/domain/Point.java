970899c33942cf31edfaec2af6c1bf8a273a9851
hwangdaesun <hds650588@gmail.com>
2025-10-24 07:54:55 +0900

    feat: 최대 출금 금액 제약 로직 추가
    - MAX_WITHDRAWAL_AMOUNT 상수 추가
    - 최대 출금 금액 초과 시 ExceedMaxWithdrawalException 예외 발생 처리

--- src/main/java/io/hhplus/tdd/point/domain/Point.java
+++ src/main/java/io/hhplus/tdd/point/domain/Point.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point.domain;
 
+import io.hhplus.tdd.point.exception.ExceedMaxWithdrawalException;
 import io.hhplus.tdd.point.exception.InsufficientPointException;
 import io.hhplus.tdd.point.exception.InvalidChargeAmountException;
 import io.hhplus.tdd.point.exception.InvalidUseAmountException;
@@ -11,6 +12,7 @@ import lombok.Getter;
 public class Point {
 
     private static final long MAX_POINT = Long.MAX_VALUE;
+    private static final long MAX_WITHDRAWAL_AMOUNT = 1_000_000L;
 
     private Member member;
     private long amount;
@@ -34,6 +36,9 @@ public class Point {
     public long use(long amount) {
         if (amount <= 0) {
             throw new InvalidUseAmountException();
+        }
+        if (amount > MAX_WITHDRAWAL_AMOUNT) {
+            throw new ExceedMaxWithdrawalException();
         }
         if (amount % 100 != 0) {
             throw new InvalidUseUnitException();
